faddea9ff498567ae6beb1a07c10c792fdd31a35
cirecirecire <eafields2010@gmail.com>
2021-12-18 13:28:40 -0500

    added new environment variable in terminal

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -46,6 +46,10 @@ def create_app(test_config=None):
     app = Flask(__name__)
 
     app.config.from_pyfile("config.py", silent=False)
+
+    app.config["MONGODB_HOST"] = os.getenv("MONGODB_HOST")
+    
+    
     if test_config is not None:
         app.config.update(test_config)
 
@@ -62,5 +66,3 @@ def create_app(test_config=None):
     login_manager.login_view = "users.login"
 
     return app
-
-app = create_app()
